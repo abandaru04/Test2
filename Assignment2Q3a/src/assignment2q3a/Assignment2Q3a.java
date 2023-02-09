522280adcaa41f26335de52288b2800ef0c66580
akbandaru <105567376+akbandaru@users.noreply.github.com>
2023-02-08 22:42:57 -0500

    second commit

---
+++ Assignment2Q3a/src/assignment2q3a/Assignment2Q3a.java
@@ -0,0 +1,38 @@
+/*
+Name: Akhilesh Bandaru
+Date: Friday January 17th 2023
+Assignment: 2
+Question: 3a
+Decription: output for x = 5 and y = 8 is as follows:
+@@@@@
+$$$$$
+&&&&&
+ */
+package assignment2q3a;
+
+/**
+ *
+ * @author akhil
+ */
+public class Assignment2Q3a {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 8;
+        
+        if (y == 8){
+            System.out.println("@@@@@");
+        }
+        if (x == 5){
+            System.out.println("$$$$$");
+            System.out.println("&&&&&");
+        }
+        else{
+            System.out.println("#####");
+        }
+    }
+    
+}
